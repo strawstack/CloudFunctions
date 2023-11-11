3fecbf3e4f831f88268eefcfbeed0492d83948aa
Richard <rhshadowgreen@gmail.com>
2023-11-11 13:25:52 -0800

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/key', async (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-const port = process.env.PORT || 8080;
+const port = 8080;
 app.listen(port, () => {
    console.log(`App is running at: http://localhost:${port}`);
 });
